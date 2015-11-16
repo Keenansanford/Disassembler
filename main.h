e04a0d15d6455f90b663fc98d2fedc073d14d780
Keenan Sanford <keenansanford@me.com>
2015-11-16 14:51:20 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef	main_h
+#define	main_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* Define types and declare global variables used throughout program. */
+// (none)
+
+/* Declare functions used within program. */
+int binToDec(char string[], int begin, int end);
+int verifyMIPSInstruction(int lineNum, char string[]);
+char * processRFormat(char string[]);
+char * processIFormat(char string[]);
+char * processJFormat(char string[]);
+
+#endif
