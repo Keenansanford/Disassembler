909000b6ced51e8118c3b6268434b9fd138b68cd
Keenan Sanford <keenansanford@me.com>
2015-11-16 14:56:13 -0500

    Create processRFormat.h

---
+++ processRFormat.h
@@ -0,0 +1,13 @@
+#ifndef	processRFormat_h
+#define	processRFormat_h
+
+#include <stdio.h>
+#include <string.h>
+/* Define types and declare global variables used throughout program. */
+// (none)
+
+/* Declare functions used within program. */
+int binToDec(char string[], int begin, int end);
+char * getRegName(int regNbr);
+
+#endif
