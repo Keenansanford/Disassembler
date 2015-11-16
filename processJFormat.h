59e8154024b8bdcbe2d6ad922d74f67d89057014
Keenan Sanford <keenansanford@me.com>
2015-11-16 14:54:51 -0500

    Create processJFormat.h

---
+++ processJFormat.h
@@ -0,0 +1,12 @@
+#ifndef	processJFormat_h
+#define	processJFormat_h
+
+#include <stdio.h>
+#include <string.h>
+/* Define types and declare global variables used throughout program. */
+// (none)
+
+/* Declare functions used within program. */
+int binToDec(char string[], int begin, int end);
+
+#endif
