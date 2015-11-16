107f587ce6fe76866e74229808fb18ef180ea3b5
Keenan Sanford <keenansanford@me.com>
2015-11-16 14:53:10 -0500

    Create processIFormat.h

---
+++ processIFormat.h
@@ -0,0 +1,13 @@
+#ifndef	processIFormat_h
+#define	processIFormat_h
+
+#include <stdio.h>
+#include <string.h>
+/* Define types and declare global variables used throughout program. */
+// (none)
+
+/* Declare functions used within program. */
+int binToDec(char string[], int begin, int end);
+char * getRegName(int regNbr);
+
+#endif
